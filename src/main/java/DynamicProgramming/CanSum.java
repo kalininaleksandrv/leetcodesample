11fabdd9ddfa542b3a87edc01fc86d8866e68330
Aleksandr Kalinin <kalinin.aleksandr.v@gmail.com>
2022-08-17 21:59:23 +0300

    add can-sum dynamic way with memo

--- src/main/java/DynamicProgramming/CanSum.java
+++ src/main/java/DynamicProgramming/CanSum.java
@@ -1,18 +1,23 @@
 package DynamicProgramming;
 
+import java.util.Map;
+
 public class CanSum {
 
     /**
      * is it possible to achieve target if sum every given digit from array, every digit can be used many times
      */
 
-    public boolean isSum (int target, int[] numbers) {
+    public boolean isSum (Integer target, int[] numbers, Map<Integer, Boolean> memo) {
+        if(memo.containsKey(target)) return memo.get(target);
         if(target == 0) return true;
         if(target<0) return false;
         for (int num:numbers) {
-            boolean sum = isSum(target - num, numbers);
-            if(sum) return true;
+            boolean res = isSum(target - num, numbers, memo);
+            memo.put(num, res);
+            if(res) return true;
         }
+        memo.put(target, false);
         return false;
     }
 }
--- src/test/java/DynamicProgramming/CanSumTest.java
+++ src/test/java/DynamicProgramming/CanSumTest.java
@@ -2,6 +2,8 @@ package DynamicProgramming;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.HashMap;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class CanSumTest {
@@ -9,9 +11,9 @@ class CanSumTest {
     @Test
     void isSum() {
         CanSum canSum = new CanSum();
-        assertTrue(canSum.isSum(7, new int[]{3,4,9}));
-        assertFalse(canSum.isSum(7, new int[]{3,5,9}));
-        assertTrue(canSum.isSum(7, new int[]{2,3}));
-        assertFalse(canSum.isSum(1, new int[]{2,2,2}));
+        assertTrue(canSum.isSum(7, new int[]{3,4,9}, new HashMap<>()));
+        assertFalse(canSum.isSum(7, new int[]{3,5,9}, new HashMap<>()));
+        assertTrue(canSum.isSum(7, new int[]{2,3}, new HashMap<>()));
+        assertTrue(canSum.isSum(200, new int[]{2,2,2}, new HashMap<>()));
     }
 }
